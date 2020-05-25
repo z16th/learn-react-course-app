1dc7a4d2ee06145bdddd62b7de416ca0bce9c8b5
LoLz XVI <47308963+LoLzWTFomfg@users.noreply.github.com>
2020-05-25 03:44:34 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  resolve:{
+    extensions: ['.mjs', '.js']
+  },
+  module: {
+    rules: [
+      {test: /\.(js)$/, use: 'babel-loader'},
+      {test: /\.css$/, use: ['style-loader', 'css-loader']}
+    ]
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
+}
